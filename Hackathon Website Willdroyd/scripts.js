e781c429bd5a6341526222d1c82578fd1107587a
willdroyd <william.abashidze@gmail.com>
2026-05-23 12:42:30 +0400

    mein first commiten

---
+++ Hackathon Website Willdroyd/scripts.js
@@ -0,0 +1,28 @@
+const title = document.querySelector('h1');
+const funBox = document.querySelector('.funBox');
+const funBox2 = document.querySelector('.funBox2');
+const mainContainer = document.querySelector('.mainContainer');
+
+funBox.addEventListener('click', () => {
+    title.textContent = "File Corrupted, You Should not have clicked random buttons.";
+    funBox.style.display = "none";
+    funBox2.style.display = "none";
+    mainContainer.style.backgroundImage = "url('photos/error.png')";
+    });
+funBox2.addEventListener('click', () => {
+    title.textContent = "Thank you for clicking that button, you saved me!";
+    funBox.style.display = "none";
+    funBox2.style.display = "none";
+    });
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'r') {
+        Reset();
+    }
+});
+function Reset(){
+    title.textContent = "Title!";
+    funBox.style.display = "block";
+    funBox2.style.display = "block";
+    mainContainer.style.backgroundImage = "none";
+}
